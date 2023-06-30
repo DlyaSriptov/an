3e5bab665fda11a7e5d09878f5486f16a942b64c
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-30 13:15:08 +0300

    Add files via upload

---
+++ netconf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+netconfIpMask=A
+netconfGateway=A
+netconfDns=A
+
+# Вводим данные (IP-адрес/Маска, Шдюз, Серверы DNS)
+read -p "IP-address/subnet mask: " netconfIpMask
+read -p "Gateway: " netconfGateway
+read -p "DNS Servers: " netconfDns
+
+# Автоматическая запись введённых данных в файл 01-netcfg.yaml
+sed -i -e 's/dhcp4: yes/dhcp4: no/' /etc/netplan/01-netcfg.yaml
+sed -i -e "8a\      addresses: [$netconfIpMask]"  /etc/netplan/01-netcfg.yaml
+sed -i -e "9a\      gateway4: $netconfGateway"  /etc/netplan/01-netcfg.yaml
+sed -i -e "10a\      nameservers:"  /etc/netplan/01-netcfg.yaml
+sed -i -e "11a\        addresses: [$netconfDns]"  /etc/netplan/01-netcfg.yaml
+
+# Применяем настройки сети
+netplan --debug generate
+netplan --debug apply
+
+apt-get -y -q install openssh-server
+
+systemctl start sshd
+
+apt-get -y -q install ufw
+
+# Открываем порты для SSH
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw allow ssh
+ufw allow 22
+ufw enable
+
+reboot
