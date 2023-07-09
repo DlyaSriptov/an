b5b8a6078a696c90859605f4d1cbae3ceb3aa137
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-07-09 22:49:23 +0300

    Update peertube.sh

--- peertube.sh
+++ peertube.sh
@@ -50,12 +50,7 @@ echo -en "\033[32m Введите пароль для пользователя p
 sudo passwd peertube
 # ============================================================
 
-# 8. Даём права для nginx на каталог www/peertube
-sudo chown -R www-data:www-data /var/www/peertube
-sudo chmod -R 755 /var/www/peertube
-# ============================================================
-
-# 9. Переходим в новый каталог и создаём пользователя PostgreSQL  peertube. При появлении запроса нужно ввести пароль для нового пользователя.
+# 8. Переходим в новый каталог и создаём пользователя PostgreSQL  peertube. При появлении запроса нужно ввести пароль для нового пользователя.
 echo -en "\033[32m Введите пароль для пользователя базы данных postgres: \033[0m \n"
 cd /var/www/peertube
 sudo -u postgres psql -c "REVOKE ALL ON DATABASE postgres FROM peertube;"
@@ -66,83 +61,83 @@ sudo -u postgres createuser -P peertube
 read -p "Введите, пожалуйста, третий раз пароль пользователя базы данных postgres: " postgresPass 
 # ============================================================
 
-# 10. Создаём базу данных PostgreSQL для использования PeerTube.
+# 9. Создаём базу данных PostgreSQL для использования PeerTube.
 sudo -u postgres createdb -O peertube -E UTF8 -T template0 peertube_prod
 # ============================================================
 
-# 11. Включаем два расширения PostgreSQL, которые нужны PeerTube
+# 10. Включаем два расширения PostgreSQL, которые нужны PeerTube
 sudo -u postgres psql -c "CREATE EXTENSION pg_trgm;" peertube_prod
 sudo -u postgres psql -c "CREATE EXTENSION unaccent;" peertube_prod
 # ============================================================
 
-# 12. Запрашиваем API PeerTube для получения подробной информации о текущем выпуске PeerTube
+# 11. Запрашиваем API PeerTube для получения подробной информации о текущем выпуске PeerTube
 VERSION=$(curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4) && echo "Latest Peertube version is $VERSION"
 # ============================================================
 
-# 13. Открываем каталог peertube, создайте несколько необходимых подкаталогов
+# 12. Открываем каталог peertube, создайте несколько необходимых подкаталогов
 cd /var/www/peertube
 sudo -u peertube mkdir config storage versions
 sudo -u peertube chmod 750 config/
 # ============================================================
 
-# 14. Скачиваем последнюю версию клиента Peertube, разархивируем и удаляем оригинальный zip-архив
+# 13. Скачиваем последнюю версию клиента Peertube, разархивируем и удаляем оригинальный zip-архив
 cd /var/www/peertube/versions
 # Releases are also available on https://builds.joinpeertube.org/release
 sudo -u peertube wget "https://github.com/Chocobozzz/PeerTube/releases/download/${VERSION}/peertube-${VERSION}.zip"
 sudo -u peertube unzip peertube-${VERSION}.zip && sudo -u peertube rm peertube-${VERSION}.zip
 # ============================================================
 
-# 15. Устанавливаем PeerTube
+# 14. Устанавливаем PeerTube
 cd /var/www/peertube
 sudo -u peertube ln -s versions/peertube-${VERSION} ./peertube-latest
 cd ./peertube-latest && sudo -H -u peertube yarn install --production --pure-lockfile
 # ============================================================
 
-# 16. Копируем файл default.yaml в каталог config. Файл редактировать и обновлять нельзя
+# 15. Копируем файл default.yaml в каталог config. Файл редактировать и обновлять нельзя
 cd /var/www/peertube
 sudo -u peertube cp peertube-latest/config/default.yaml config/default.yaml
 # ============================================================
 
-# 17. Из того же каталога копируем production.yaml в новое место
+# 16. Из того же каталога копируем production.yaml в новое место
 cd /var/www/peertube
 sudo -u peertube cp peertube-latest/config/production.yaml.example config/production.yaml
 # ============================================================
 
-# 18. Редактируем файл /var/www/peertube/config/production.yaml
+# 17. Редактируем файл /var/www/peertube/config/production.yaml
 sudo sed -i -e "s|hostname: 'example.com'|hostname: '$peerTubeNameDomain'|" /var/www/peertube/config/production.yaml
 sudo sed -i -e "s|peertube: ''|peertube: '$keyScan'|" /var/www/peertube/config/production.yaml
 sudo sed -i -e "s|password: 'peertube'|password: '$postgresPass'|" /var/www/peertube/config/production.yaml
 sudo sed -i -e "s|email: 'admin@example.com'|email: '$emailScan'|" /var/www/peertube/config/production.yaml
 # ============================================================
 
-# 19. Копируем шаблон файла конфигурации NGINX для PeerTube в каталог /etc/nginx/sites-available
+# 18. Копируем шаблон файла конфигурации NGINX для PeerTube в каталог /etc/nginx/sites-available
 sudo cp /var/www/peertube/peertube-latest/support/nginx/peertube /etc/nginx/sites-available/peertube
 # ============================================================
 
-# 20. В файле /etc/nginx/sites-available/peertube меняем имя домена (1-я строка), и локальный адрес:порт (2-я строка)
+# 19. В файле /etc/nginx/sites-available/peertube меняем имя домена (1-я строка), и локальный адрес:порт (2-я строка)
 sudo sed -i 's/${WEBSERVER_HOST}/$peerTubeNameDomain/g' /etc/nginx/sites-available/peertube
 sudo sed -i 's/${PEERTUBE_HOST}/127.0.0.1:9000/g' /etc/nginx/sites-available/peertube
 # ============================================================
 
-# 21. Создаём ссылку на конфиг. файл peertube в каталоге /etc/nginx/sites-enabled
+# 20. Создаём ссылку на конфиг. файл peertube в каталоге /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/peertube /etc/nginx/sites-enabled/peertube
 # ============================================================
 
-# 22. Установка сертификата Let's Encrypt
+# 21. Установка сертификата Let's Encrypt
 sudo systemctl stop nginx
 sudo certbot certonly --standalone --post-hook "systemctl restart nginx"
 sudo systemctl reload nginx
 # ============================================================
 
-# 23. Скопируйте файл 30-peertube-tcp.conf в каталог с системными настройками
+# 22. Скопируйте файл 30-peertube-tcp.conf в каталог с системными настройками
 sudo cp /var/www/peertube/peertube-latest/support/sysctl.d/30-peertube-tcp.conf /etc/sysctl.d/
 # ============================================================
 
-# 24. Запускаем ранее скопированный файл
+# 23. Запускаем ранее скопированный файл
 sudo sysctl -p /etc/sysctl.d/30-peertube-tcp.conf
 # ============================================================
 
-# 25. Завершающие настройки и запуск службы PeerTube
+# 24. Завершающие настройки и запуск службы PeerTube
 sudo cp /var/www/peertube/peertube-latest/support/systemd/peertube.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable peertube
